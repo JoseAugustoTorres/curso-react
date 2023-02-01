faa66443e731ca8e4d4b8c0baa0b55260458c538
Jose <jose.torress@hotmail.com>
2023-02-01 20:17:33 -0300

    feat: adicionando props aos Pokemons

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export const Pokemon = () => {
-    return <h2>Nome do Pokemon: Pikachu</h2>;
+export const Pokemon = ({ name = "pokemon", tipo = "tipo"}) => {
+    return (
+    <div>
+        <h2>Nome do Pokemon: {name}.</h2>
+        <h3>Tipo do pokemon: {tipo}</h3>
+    </div>);
   };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,11 @@ export const Home = () => {
   return (
     <div>
       <h1>Meu site de Pokemon</h1>
+      <Pokemon name="Pikachu" tipo="Elétrico" />
+      <Pokemon name="Charizard" tipo="Fogo" />
+      <Pokemon name="Squirtle" tipo="Água" />
+      <Pokemon name="Bulbassauro" tipo="Planta" />
+      <Pokemon name="Charmander" tipo="Fogo" />
       <Pokemon />
     </div>
   );
