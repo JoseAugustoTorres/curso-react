3056180f17dcea72745e7022e6fb9181a1e3907d
Jose <jose.torress@hotmail.com>
2023-02-01 19:34:25 -0300

    feat: Criando componente Pokemon e criando pastas pages e components

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Pokemon = () => {
+    return <h2>Nome do Pokemon: Pikachu</h2>;
+  };
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Pokemon } from "../components/Pokemon";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Meu site de Pokemon</h1>
+      <Pokemon />
+    </div>
+  );
+};
