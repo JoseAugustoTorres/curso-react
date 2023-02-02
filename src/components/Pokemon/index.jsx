9234f72dc1cdb85caa4cfd2866828cb037eeed6d
Jose <jose.torress@hotmail.com>
2023-02-02 14:13:21 -0300

    feat: nova estrutura de pastas

--- src/components/Pokemon/index.jsx
+++ src/components/Pokemon/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Pokemon = ({ name = "pokemon", tipo = "tipo"}) => {
     return (
     <div>
-        <h2>Nome do Pokemon: {name}.</h2>
+        <h2 >Nome do Pokemon: {name}.</h2>
         <h3>Tipo do pokemon: {tipo}</h3>
     </div>);
   };
